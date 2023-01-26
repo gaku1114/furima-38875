eed11e42aa8d945ee4ebc6547e1ae3c05f776427
gaku1114 <ga1114ku@gmail.com>
2023-01-27 01:40:06 +0900

    itemモデルのバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  validates :name,          presence: true
+  validates :description,   presence: true
+  validates :category_id,   numericality: { other_than: 1 } 
+  validates :condition_id,  numericality: { other_than: 1 } 
+  validates :postage_id,    numericality: { other_than: 1 } 
+  validates :deadline_id,   numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :price,         presence: true, numericality: { in: 300..9999999 }, format: { with:  /\A[0-9]+\z/ }
+
 end
