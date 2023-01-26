c055048682aabc8e736f04cde5635ed47cd83f06
gaku1114 <ga1114ku@gmail.com>
2023-01-27 01:47:51 +0900

    itemモデルのアソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,12 @@ class Item < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 1 } 
   validates :price,         presence: true, numericality: { in: 300..9999999 }, format: { with:  /\A[0-9]+\z/ }
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :deadline
+  belongs_to :prefecture 
+
+  belongs_to :user
 end
