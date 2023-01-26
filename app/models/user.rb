d0ef4b40421e6efa8272ecb946249ea84c09e1f0
gaku1114 <ga1114ku@gmail.com>
2023-01-26 12:41:42 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :encrypted_password, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :nickname, presence: true
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
