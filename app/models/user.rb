7d1cf776bbedec8a154343e4d4844f54c42666d1
gaku1114 <ga1114ku@gmail.com>
2023-01-26 12:05:58 +0900

    トップページからdeviseのページに遷移する

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :encrypted_password, format: { with: VALID_PASSWORD_REGEX }
   validates :nickname, presence: true
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
