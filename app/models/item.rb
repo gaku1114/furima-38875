771e36094fe8df45947e7ad62203232e9748d87b
gaku1114 <ga1114ku@gmail.com>
2023-01-27 16:34:57 +0900

    activehashのエラーメッセージを変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
   validates :name,          presence: true
   validates :description,   presence: true
-  validates :category_id,   numericality: { other_than: 1 } 
-  validates :condition_id,  numericality: { other_than: 1 } 
-  validates :postage_id,    numericality: { other_than: 1 } 
-  validates :deadline_id,   numericality: { other_than: 1 } 
-  validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :price,         presence: true, numericality: { in: 300..9999999 }, format: { with:  /\A[0-9]+\z/ }
+  validates :category_id,   numericality: { other_than: 1, message: 'を選択してください' } 
+  validates :condition_id,  numericality: { other_than: 1, message: 'を選択してください' } 
+  validates :postage_id,    numericality: { other_than: 1, message: 'を選択してください' } 
+  validates :deadline_id,   numericality: { other_than: 1, message: 'を選択してください' } 
+  validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' } 
+  validates :price,         presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with:  /\A[0-9]+\z/ }
   validates :image,         presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
