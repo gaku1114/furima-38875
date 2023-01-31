ca6c6d4b1cc04072df62cb962ef0fe78f35d2e3c
gaku1114 <ga1114ku@gmail.com>
2023-02-01 02:38:41 +0900

    売却済み商品の場合はトップページに遷移する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :item_judge
 
   def new
     @item = Item.find(params[:item_id])
@@ -17,10 +18,15 @@ class OrdersController < ApplicationController
     end
   end
     
-   
-
   private
   def order_params
     params.require(:order_destination).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def item_judge
+    item = Item.find(params[:item_id])
+    if current_user == item.user || Order.exists?(item_id: item.id)
+      redirect_to root_path
+    end
   end
 end
